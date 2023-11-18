9b039994ed9fbdd3e07c972d93abef1d1c8d2966
Simon Banka <szymon.banka@gmail.com>
2023-11-18 17:29:26 -0600

    p

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<A-h>", ":vertical 1resize -5<cr>", { desc = "Decrease Neotree width" })
+vim.keymap.set("n", "<A-l>", ":vertical 1resize +5<cr>", { desc = "Increase Neotree width" })
