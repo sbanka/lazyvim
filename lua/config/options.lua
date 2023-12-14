1057f1e961bc67197a71df5ec50cd3af2b0db5a7
Szymon Banka <szymon.banka@gmail.com>
2023-12-13 18:31:00 -0600

    disable osc52 in config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,14 +7,14 @@ vim.opt.guifont = { "JetBrainsMono Nerd Font", "h19" }
 vim.opt.clipboard = "unnamedplus"
 -- vim.opt.inccommand = "split"
 
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.clipboard.osc52").copy,
-    ["*"] = require("vim.clipboard.osc52").copy,
-  },
-  paste = {
-    ["+"] = require("vim.clipboard.osc52").paste,
-    ["*"] = require("vim.clipboard.osc52").paste,
-  },
-}
+-- vim.g.clipboard = {
+--   name = "OSC 52",
+--   copy = {
+--     ["+"] = require("vim.clipboard.osc52").copy,
+--     ["*"] = require("vim.clipboard.osc52").copy,
+--   },
+--   paste = {
+--     ["+"] = require("vim.clipboard.osc52").paste,
+--     ["*"] = require("vim.clipboard.osc52").paste,
+--   },
+-- }
